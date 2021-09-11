22da85f76f7d0b3d3e40ea19bd9ea113d688fd3c
MALGORZATA BSLCERZAK <malgorzatabslcerzak@MALGORZATAs-MacBook-Air.local>
2021-09-11 20:00:57 +0200

    graph for pred and true

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from altair.vegalite.v4.api import concat
 import streamlit as st
 import json
 import pandas as pd
@@ -23,41 +24,34 @@ def get_vacc_rates():
     with open("data/vacc_rates_county.json", "r") as f:
        return json.load(f)
 
+def get_county_df(df, county, colname):
+    df = df[county]
+    dates = list(df)
+    cases = df.values()
+    return pd.DataFrame(list(zip(dates, cases)), columns=['date',colname])
+
 
 counties = get_locations_json()
 vacc_rates = get_vacc_rates()
 
 county_selected = st.sidebar.selectbox('Select county FIPS number: ', sorted(counties, key=int))
-
-data = get_true_cases()
-data = data[county_selected]
-dates = list(data)
-cases = data.values()
-
-df = pd.DataFrame(list(zip(dates, cases)), columns=['date','value'])
-df['date'] = pd.to_datetime(df['date'],format='%Y-%m-%d', errors='ignore')
-df = df.rename(columns={'date':'index'}).set_index('index')
-
-# date_range = st.slider('Select a date range', min(dates), max(dates), (min(dates), max(dates)))
-
+m = st.sidebar.markdown("[by MAB](https://github.com/mbalcerzak)")
 county_name = counties[county_selected]
 
-st.title('Recorded new cases')
-st.header(f'Actual cases: {county_name}')
+data_t = get_true_cases()
+data_true = get_county_df(data_t, county_selected, 'value_true')
 
-st.line_chart(df)
+data_p = get_predicted_cases()
+data_pred = get_county_df(data_p, county_selected, 'value_pred')
 
-data_pred = get_predicted_cases()
-data_pred = data_pred[county_selected]
-dates_pred = list(data_pred)
-cases_pred = data_pred.values()
+st.header(f'Predicted and True cases: {county_name}')
+st.write("Model selected: CEID-Walk")
 
-df_pred = pd.DataFrame(list(zip(dates_pred, cases_pred)), columns=['date','value'])
-df_pred['date'] = pd.to_datetime(df_pred['date'],format='%Y-%m-%d', errors='ignore')
-df_pred = df_pred.rename(columns={'date':'index'}).set_index('index')
+df_both = pd.merge(left=data_true, right=data_pred, on='date', how='inner')
+df_both['date'] = pd.to_datetime(df_both['date'],format='%Y-%m-%d', errors='ignore')
+df_both = df_both.rename(columns={'date':'index'}).set_index('index')
 
-st.header('Predicted cases (model: CEID-Walk)')
-st.line_chart(df_pred)
+st.line_chart(df_both)
 
 st.subheader('Vaccination rates for the area')
 st.write(f'Fully vaccinated in {county_name}: {vacc_rates[county_selected]} %')
